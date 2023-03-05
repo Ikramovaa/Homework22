5cae3c0b5ce6a4413da7dfd7294b2ac080058683
админ <missaidaikramova@gmail.com>
2023-03-06 00:38:09 +0600

    Initial commit

---
+++ src/Song.java
@@ -0,0 +1,34 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Song {
+    public  void  fileReader()throws IOException{
+        try{
+            FileReader fileReader=new FileReader("Metallica.txt");
+            Scanner file=new Scanner(fileReader);
+            while (file.hasNextLine()){
+                System.out.println(file.next());
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public  void fileWriter() throws IOException {
+        FileWriter fileWriter = new FileWriter("Metallica.txt");
+        fileWriter.write("str:----Nothing else matter----" +
+                "\n Author: Metallica" +
+                "\n So close,no matter ow far" +
+                "\n Couldn't be much more from the heart" +
+                "\n Forever trusting who we are" +
+                "\n And nothing else matters" +
+                "\n And never opened myself this way" +
+                "\n Life is ours,we live it our way" +
+                "\n All these words I don't just say" +
+                "\n And nothing else matters");
+        fileWriter.close();
+
+    }
+}
